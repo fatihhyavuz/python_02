4faf7a3825dd6131192d89aab0bdd95475bc2c69
fatihhyavuz <fatihyvz993@gmail.com>
2024-04-27 12:59:37 +0300

    v1

--- practice02.py
+++ practice02.py
@@ -17,7 +17,7 @@ print(type(e))
 
 text = "hello,my,name.is tom."
 text.upper()
-text.replace("," ,  " ").replace("."," ")
+text.replace("," ,  " ").replace("."," ").split()
 
 # 03
 
@@ -25,13 +25,14 @@ list=["d","a","t","a","s","c","i","e","n","c","e"]
 list2 = []
 
 len(list)
+
 print(list[0])
 print(list[3])
 
 list.pop(8)
 
 
-list2.append(list[0:4])
+list2 = list[0:4]
 print(list2)
 
 list.append("a")
@@ -47,12 +48,18 @@ dict= {"tom":["use",19],
 
 print(dict.keys())
 print(dict.values())
-dict.update()
+dict.update({"tom":["england",13]})
+
+dict.update({"ahmet":["turk",45]})
+
+dict.pop("tom")
+print(dict)
 
 #04
 
 cift = []
 tek = []
+
 def func(list):
   for new_list in list:
     if new_list % 2 == 0:
@@ -69,10 +76,12 @@ print(tek)
 
 students = ["kirk","jon","adam","tom","mary","andrew"]
 
-for index, student in enumerate(students, start=1):
-  if index <= 3:
+for index, student in enumerate(students):
+  if index <= 2:
+    index += 1 
     print("enginering " + str(index)+". student: "+ student)
   else: 
+     index -= 2 
      print("medicine " + str(index)+". student: "+student) 
 
 # 06 
@@ -83,7 +92,23 @@ limit_of_number = [23,56,12,34]
 
 
 
-new = zip(pin_of_leasson,name_of_leasson,limit_of_number)
+for pin_of_leasson,name_of_leasson,limit_of_number in zip(pin_of_leasson,name_of_leasson,limit_of_number):
+  print(f"adi {name_of_leasson} olan {pin_of_leasson} kodlu dersin konteyjani {limit_of_number} kisidir.")
 
-new_new= list(new)
-print(new)
+# 07
+
+kum1 = set(["data","python"])
+kum2 = set(["data","function","qcut","lambda","python","miuul"])
+
+def new_kum(kum1,kum2):
+  if kum1.issuperset(kum2) == True :
+    i_new =kum1.intersection(kum2)
+    print(i_new)
+  else: 
+    a_new = kum2.difference(kum1)
+    print(a_new) 
+
+  return new_kum
+new_kum(kum1,kum2)
+new_kum(kum2,kum1)
+
